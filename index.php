38d8ae31c4c009b4c70532a99ce20d7cdbc01717
Dylan <dylanjiawei@gmail.com>
2021-11-21 14:56:53 +0800

    updated version of the portfolio

--- index.php
+++ index.php
@@ -253,6 +253,7 @@ if($_POST) {
         </div>
     </main>
     <script src="src/js/nav.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script>
         $("form").submit(function(e){
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-import './style.css'
-import gsap from 'gsap';
+import * as gsap from 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.8.0/gsap.min.js';
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
